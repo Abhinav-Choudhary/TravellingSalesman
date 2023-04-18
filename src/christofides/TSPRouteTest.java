17a59abb8ef3ccf54f94f5bef7cb145a99d7445d
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2023-04-18 12:39:23 -0400

    checking maven test cases

---
+++ src/christofides/TSPRouteTest.java
@@ -0,0 +1,14 @@
+package christofides;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TSPRouteTest {
+
+	@Test
+	void testBuild() {
+		fail("Not yet implemented");
+	}
+
+}
